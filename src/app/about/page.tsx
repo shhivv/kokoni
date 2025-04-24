2fb3158d740ad15907e03ed4bbe186e013605edc
Shiv <shiv.7.shan@gmail.com>
2025-04-24 23:46:36 +0530

    update about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,7 +3,7 @@ export default async function Page() {
 
   return (
     <main className="h-[calc(100vh-4rem)] w-full bg-card">
-
+Kokoni is an open-source research assistant powered by AI artificial intelligence that helps you pick and organize topics to do comprehensive research on anything you want to learn about.
     </main>
   );
 }
