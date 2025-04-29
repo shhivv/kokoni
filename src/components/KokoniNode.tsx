5a8eecc89f310118d2141683154a2bebc88da055
Shiv <shiv.7.shan@gmail.com>
2025-04-29 23:09:44 +0530

    fix connectable

--- src/components/KokoniNode.tsx
+++ src/components/KokoniNode.tsx
@@ -8,7 +8,7 @@ export const KokoniNode = memo(({ data }: { data: Node}) => {
     <div 
       className={`w-72 ${data.selected ? 'py-8': 'py-4'} px-4 shadow-md rounded-md bg-muted border border-neutralborder`}
     >
-      { data.parentId ? <Handle type="target" position={Position.Left} className="w-2 h-2" />: null }
+      { data.parentId ? <Handle type="target" position={Position.Left} isConnectable={false} className="w-2 h-2" />: null }
       <div className="flex flex-col">
         <div className={`text-sm font-medium ${data.selected ? 'text-foreground' : 'text-muted-foreground'}`}>{data.question}</div>
         { data.summary &&  <div className="text-xs text-neutral-500 pt-4">{data.summary}</div> }
